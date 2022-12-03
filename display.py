2a77928186cb1d088a089b1f677a468ea0034b76
Linden <Lindenmw2@gmail.com>
2022-12-03 18:38:24 +0000

    drawing of crosses and circles

--- display.py
+++ display.py
@@ -9,6 +9,7 @@ BLACK = (0, 0, 0)
 CELLS_WIDTH = 3
 CELLS_HEIGHT = 3
 
+player_1_turn = True 
 screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 running = True
 
@@ -22,7 +23,21 @@ def draw_line(cells_width, cells_height, screen: pg.SurfaceType):
     for i in range(cells_height -1):
         pg.draw.line(screen, BLACK, (0, (i+1)*screen.get_width()/cells_height-1), (600, (i+1)*screen.get_width()/cells_height-1), 2)
 
+def draw_turn(player_1_turn, cells_width, cells_height, cell_x_pos, cell_y_pos, screen: pg.SurfaceType):
+    cell_width = screen.get_width() / cells_width
+    cell_height = screen.get_height() / cells_height
+    if player_1_turn:
+        pg.draw.circle(screen, BLACK, (cell_x_pos * cell_width + cell_width / 2, cell_y_pos * cell_height + cell_height / 2), 0.8 * screen.get_width()/(cells_width * 2), 2)
+    else:
+        for i in range(2):
+            pg.draw.line(screen, BLACK, (cell_x_pos * cell_width + cell_width / 5, cell_y_pos * cell_height + cell_height / 5), (cell_x_pos * cell_width + cell_width * 4 / 5, cell_y_pos * cell_height + cell_height * 4 / 5), 2)
+            pg.draw.line(screen, BLACK, (cell_x_pos * cell_width + cell_width * 4 / 5, cell_y_pos * cell_height + cell_height / 5), (cell_x_pos * cell_width + cell_width / 5, cell_y_pos * cell_height + cell_height * 4 / 5), 2)
+
 draw_line(CELLS_WIDTH, CELLS_HEIGHT, screen)
+
+draw_turn(False, CELLS_WIDTH, CELLS_HEIGHT, 1, 2, screen)
+
+draw_turn(True, CELLS_WIDTH, CELLS_HEIGHT, 0, 1, screen)
 
 #game loop
 while running:
